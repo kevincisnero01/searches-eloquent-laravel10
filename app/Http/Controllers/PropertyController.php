f73ca7bb66e264f121a22fa40ab0ecdb028e75a3
kevincisnero01 <kevingabriel18@gmail.com>
2023-05-14 22:27:18 -0400

    feat(filter):  search optimize
    -instantiate query from query builder
    -search for matching terms.
    -optional and dynamic searches
    -search properties from their relations.
    -applying a query through a many-to-many relationship

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -9,10 +9,22 @@ class PropertyController extends Controller
 {
     public function filter(Request $request, Property $property)
     {
-        if($request->id) $property = $property->where('id',$request->id);
 
-        if($request->name) $property = $property->where('name', $request->name);
+        $query = $property->newQuery();
 
-        return response()->json($property->get());
+        if($request->id) 
+            $query->where('id',$request->id);
+
+        if($request->name) 
+            $query->where('name','like','%'.$request->name.'%');
+
+        if($request->features)
+        {
+            $query->whereHas('features', function($q) use ($request){
+                $q->whereIn('features.id', $request->features);
+            });
+        }
+
+        return response()->json($query->get());
     }
 }
