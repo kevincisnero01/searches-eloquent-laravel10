a741b982575b9a728a27faf30d058e53f357ea89
kevincisnero01 <kevingabriel18@gmail.com>
2023-05-14 21:00:16 -0400

    feat(filter):  endpoint filter
    -function filter in controller
    -add exception token csrf
    -search by dynamic parameters

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    //
+    public function filter(Request $request, Property $property)
+    {
+        if($request->id) $property = $property->where('id',$request->id);
+
+        if($request->name) $property = $property->where('name', $request->name);
+
+        return response()->json($property->get());
+    }
 }
